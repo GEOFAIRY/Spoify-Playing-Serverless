e6b70cb7b1b52bc27acfa433bf3754b58c53a1d7
GEOFAIRY <krs19@live.com>
2021-07-20 00:12:07 +1200

    begag shift to azure i have no idea what im doing XDDDDD

--- index.js
+++ index.js
@@ -1,67 +1,14 @@
-require('dotenv').config()
-
-const express = require('express')
-const PORT = 3000
-
 const axios = require('axios')
-// axios.interceptors.request.use((request) => {
-//     console.log('Starting Request', JSON.stringify(request, null, 2))
-//     return request
-// })
 
-// axios.interceptors.response.use((response) => {
-//     console.log('Response:', JSON.stringify(response, null, 2))
-//     return response
-// })
+const _REFRESHTOKEN = process.env['spotify_refresh_token']
+const _CLIENTSECRET = process.env['spotify_client_secret']
+const _CLIENTID = process.env['spotify_client_id']
 
-const _REFRESHTOKEN = process.env.spotify_refresh_token
-const _CLIENTSECRET = process.env.spotify_client_secret
-const _CLIENTID = process.env.spotify_client_id
-
-var app = express()
-app.listen(PORT, () => {
-    console.log('listening on', PORT)
-})
-
-app.get('/current-playing', async (req, res) => {
-    var token = await refreshToken()
-
-    var headers = { Authorization: 'Bearer ' + token }
-    axios({
-        method: 'get',
-        url: 'https://api.spotify.com/v1/me/player/currently-playing',
-        headers: headers,
-    })
-        .then(async function (spotify_res_current) {
-            if (spotify_res_current.status === 204) {
-                res = await getrecent(res)
-            } else {
-                // now playing
-                res.json({
-                    album: {
-                        name: spotify_res_current.data.item.album.name,
-                        image: spotify_res_current.data.item.album.images[0]
-                            .url,
-                        link: spotify_res_current.data.item.album.external_urls
-                            .spotify,
-                    },
-                    artist: {
-                        name: spotify_res_current.data.item.artists[0].name,
-                        link: spotify_res_current.data.item.artists[0]
-                            .external_urls.spotify,
-                    },
-                    track: {
-                        name: spotify_res_current.data.item.name,
-                        link: spotify_res_current.data.item.external_urls
-                            .spotify,
-                    },
-                })
-            }
-        })
-        .catch(function (err) {
-            console.error(err)
-        })
-})
+function getAuth() {
+    var str = _CLIENTID + ':' + _CLIENTSECRET
+    const buffer = Buffer.from(str, 'utf-8')
+    return buffer.toString('base64')
+}
 
 async function refreshToken() {
     var access_token
@@ -91,10 +38,52 @@ async function refreshToken() {
     return access_token
 }
 
-function getAuth() {
-    var str = _CLIENTID + ':' + _CLIENTSECRET
-    const buffer = Buffer.from(str, 'utf-8')
-    return buffer.toString('base64')
+async function getPlaying() {
+    res = {}
+    var token = await refreshToken()
+    var headers = { Authorization: 'Bearer ' + token }
+
+    axios({
+        method: 'get',
+        url: 'https://api.spotify.com/v1/me/player/currently-playing',
+        headers: headers,
+    })
+        .then(async function (spotify_res_current) {
+            if (spotify_res_current.status === 204) {
+                res = await getrecent(res)
+            } else {
+                // now playing
+                res = {
+                    status: 200,
+                    body: {
+                        album: {
+                            name: spotify_res_current.data.item.album.name,
+                            image: spotify_res_current.data.item.album.images[0]
+                                .url,
+                            link: spotify_res_current.data.item.album
+                                .external_urls.spotify,
+                        },
+                        artist: {
+                            name: spotify_res_current.data.item.artists[0].name,
+                            link: spotify_res_current.data.item.artists[0]
+                                .external_urls.spotify,
+                        },
+                        track: {
+                            name: spotify_res_current.data.item.name,
+                            link: spotify_res_current.data.item.external_urls
+                                .spotify,
+                        },
+                    },
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            }
+        })
+        .catch(function (err) {
+            console.error(err)
+        })
+    return res
 }
 
 async function getrecent(res) {
@@ -133,4 +122,18 @@ async function getrecent(res) {
             console.error(err)
         })
     return res
+}
+
+module.exports = async function (context, req) {
+    try {
+        // do the thing
+        var res = await getPlaying()
+        context.done(null, res)
+    } catch (error) {
+        const err = JSON.stringify(error)
+        context.res = {
+            status: 500,
+            body: `Request error. ${err}`,
+        }
+    }
 }
